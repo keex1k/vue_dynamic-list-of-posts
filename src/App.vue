1c4cf316230bda37f3a8deba04bb7bf7ea9903fb
Bartek <kowalski_1410@wp.pl>
2025-08-10 11:27:19 +0200

    first commit - add project structure

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<template>
+</template>
+
+<style>
+.list-enter-active,
+.list-leave-active {
+  max-height: 60px;
+  /* transition: all 0.5s ease; */
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  max-height: 0;
+  transform: scaleY(0);
+}
+</style>
