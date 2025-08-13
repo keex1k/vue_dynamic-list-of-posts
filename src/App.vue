435f92796a3789bef48154f403322da5af09f2ec
Bartek <kowalski_1410@wp.pl>
2025-08-13 23:20:46 +0200

    add adding&editing&deleting post

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,85 @@
 <script>
-import * as usersApi from './api/users.js'
-import * as postsApi from './api/posts.js'
-import AppHeader from './components/Header.vue';
-import PostsList from './components/PostList.vue';
-import Loader from './components/Loader.vue'
+import * as usersApi from "./api/users.js";
+import * as postsApi from "./api/posts.js";
+import AppHeader from "./components/Header.vue";
+import PostsList from "./components/PostList.vue";
+import Loader from "./components/Loader.vue";
+import AddPostPopup from "./components/AddPostPopup.vue";
 
 export default {
-  components: {
-    AppHeader,
-    PostsList,
-    Loader
-  },
+  components: { AppHeader, PostsList, Loader, AddPostPopup },
 
   data() {
     return {
       posts: [],
       user: null,
       isLoading: true,
+    };
+  },
+
+  async mounted() {
+    try {
+      const [postsRes, userRes] = await Promise.all([
+        postsApi.getPostsByUserId(3032),
+        usersApi.getUserbyEmail("kowalski_1410@wp.pl"),
+      ]);
+      this.posts = postsRes.data;
+      this.user = userRes.data?.[0] || null;
+    } catch (e) {
+      console.error(e);
+    } finally {
+      this.isLoading = false;
     }
   },
 
-  mounted() {
-  postsApi.getPostsByUserId(589)
-    .then(res => {
-      this.posts = res.data; 
-      this.isLoading = false;
-    })
-  
-  usersApi.getUserbyEmail("kowalski_1410@wp.pl")
-    .then(res => {
-      this.user = res.data[0]
-    })
+  methods: {
+    async addPost(postData, done) {
+      const res = await postsApi.createPost({
+        title: postData.title,
+        body: postData.body,
+        userId: this.user.id,
+      });
+
+      this.posts.unshift(res.data);
+
+      if (typeof done === "function") {
+        done(res.data);
+      }
+    },
+
+    async removePost(postId) {
+      await postsApi.deletePost(postId);
+      this.posts = this.posts.filter((post) => post.id !== postId);
+    },
+
+    async editPost(postData) {
+      const res = await postsApi.updatePost(postData.id, {
+        title: postData.title,
+        body: postData.body,
+      });
+
+      // Zaktualizuj lokalnie listę postów
+      this.posts = this.posts.map((post) =>
+        post.id === postData.id ? { ...post, ...res.data } : post
+      );
+    },
   },
 };
-
 </script>
 
 <template>
-  <AppHeader :user="user"/>
+  <AppHeader :user="user" />
 
   <main class="section">
     <div class="container">
       <div class="tile is-ancestor is-flex is-flex-wrap-wrap">
-        <PostsList :posts="posts" :isLoading="isLoading"/>
+        <PostsList
+          :posts="posts"
+          :isLoading="isLoading"
+          @add="addPost"
+          @delete="removePost"
+          @edit="editPost"
+        />
       </div>
     </div>
   </main>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,87 +1,159 @@
 <script>
-import Sidebar from './Sidebar.vue'
-import Loader from './Loader.vue'
+import Sidebar from "./Sidebar.vue";
+import Loader from "./Loader.vue";
+import AddPostPopup from "./AddPostPopup.vue";
+import EditPostPopup from "./EditPostPopup.vue";
 
 export default {
-    components: {
-        Sidebar,
-        Loader
+  components: {
+    Sidebar,
+    Loader,
+    AddPostPopup,
+    EditPostPopup,
+  },
+  name: "PostsList",
+  props: {
+    posts: {
+      type: Array,
+      required: true,
     },
-    name: 'PostsList',
-    props: {
-        posts: {
-            type: Array,
-            required: true
-        },
-        isLoading: Boolean
+    isLoading: Boolean,
+  },
+  data() {
+    return {
+      selectedPostId: 0,
+      addPopupOpened: false,
+      editPopupOpened: false,
+    };
+  },
+  emits: ["add", "delete", "edit"],
+
+  methods: {
+    toggleSelectedPost(id) {
+      if (this.selectedPostId === id) {
+        this.selectedPostId = 0;
+      } else {
+        this.selectedPostId = id;
+      }
+
+      if (this.addPopupOpened) {
+        this.addPopupOpened = false;
+      }
     },
-    data() {
-        return {
-            selectedPostId: 0,
-        }
+    openAddPostPopup() {
+      if (this.selectedPostId !== 0) {
+        this.selectedPostId = 0;
+      }
+
+      if (this.addPopupOpened) {
+        this.addPopupOpened = false;
+      } else {
+        this.addPopupOpened = true;
+      }
     },
-    methods: {
-        toggleSelectedPost(id) {
-            if (this.selectedPostId === id) {
-                this.selectedPostId = 0
-            } else {
-                this.selectedPostId = id;
-            }
-        }
+    openEditPostPopup() {
+      this.editPopupOpened = true;
     },
-    computed: {
-        selectedPost() {
-            return this.posts.find(p => p.id === this.selectedPostId);
-        }
-    }
-}
+    closeEditPostPopup() {
+      this.editPopupOpened = false;
+    },
+    closeAddPopup() {
+      this.addPopupOpened = false;
+    },
+    preDelete(postId) {
+      this.$emit("delete", postId);
+    },
+    forwardAdd(postData) {
+      this.$emit("add", postData, (newPost) => {
+        this.selectedPostId = newPost.id;
+      });
+      this.openAddPostPopup();
+    },
+    preEdit(postData) {
+      this.$emit("edit", {
+        id: this.selectedPostId,
+        title: postData.title,
+        body: postData.body,
+      });
+      this.closeEditPostPopup();
+    },
+  },
+  computed: {
+    selectedPost() {
+      return this.posts.find((p) => p.id === this.selectedPostId);
+    },
+  },
+};
 </script>
 
 <template>
-    <div class="tile is-parent">
-        <div class="tile is-child box is-success">
-            <div class="block">
-                <div class="block is-flex is-justify-content-space-between">
-                    <p class="title">Posts</p>
-                    <button type="button" class="button is-link">Add New Post</button>
-                </div>
-
-                <table class="table is-fullwidth is-striped is-hoverable is-narrow">
-                    <thead>
-                        <tr class="has-background-link-light">
-                            <th>ID</th>
-                            <th>Title</th>
-                            <th class="has-text-right">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody v-if="isLoading">
-                        <tr>
-                            <td colspan="3" class="has-text-centered" style="padding: 2rem;">
-                                <Loader />
-                            </td>
-                        </tr>
-                    </tbody>
-
-                    <tbody v-else>
-                        <tr v-for="post in posts" :key="post.id">
-                            <td>{{ post.id }}</td>
-                            <td>{{ post.title }}</td>
-                            <td class="has-text-right is-vcentered">
-                                <button type="button" class="button is-link"
-                                    :class="{ 'is-light': post.id === selectedPostId }"
-                                    @click="toggleSelectedPost(post.id)">
-                                    {{ post.id === selectedPostId ? 'Close' : 'Open' }}
-                                </button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+  <div class="tile is-parent">
+    <div class="tile is-child box is-success">
+      <div class="block">
+        <div class="block is-flex is-justify-content-space-between">
+          <p class="title">Posts</p>
+          <button
+            type="button"
+            class="button is-link"
+            @click="openAddPostPopup"
+          >
+            Add New Post
+          </button>
         </div>
+
+        <table class="table is-fullwidth is-striped is-hoverable is-narrow">
+          <thead>
+            <tr class="has-background-link-light">
+              <th>ID</th>
+              <th>Title</th>
+              <th class="has-text-right">Actions</th>
+            </tr>
+          </thead>
+          <tbody v-if="isLoading">
+            <tr>
+              <td colspan="3" class="has-text-centered" style="padding: 2rem">
+                <Loader />
+              </td>
+            </tr>
+          </tbody>
+
+          <tbody v-else>
+            <tr v-for="post in posts" :key="post.id">
+              <td>{{ post.id }}</td>
+              <td>{{ post.title }}</td>
+              <td class="has-text-right is-vcentered">
+                <button
+                  type="button"
+                  class="button is-link"
+                  :class="{ 'is-light': post.id === selectedPostId }"
+                  @click="toggleSelectedPost(post.id)"
+                >
+                  {{ post.id === selectedPostId ? "Close" : "Open" }}
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
+  </div>
 
-    <Sidebar v-if="selectedPost" :post="selectedPost" />
+  <AddPostPopup
+    v-if="addPopupOpened"
+    @add="forwardAdd"
+    @close="closeAddPopup"
+  />
+  <Sidebar
+    v-if="selectedPost && !editPopupOpened"
+    :post="selectedPost"
+    @delete="preDelete"
+    @open="openEditPostPopup"
+  />
+  <EditPostPopup
+    v-if="selectedPost && editPopupOpened"
+    :post="selectedPost"
+    @close="closeEditPostPopup"
+    @edit="preEdit"
+  />
 </template>
-<style>
-
-</style>
+<style></style>
